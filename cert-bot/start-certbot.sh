bff3cd90a4e8a4e16b9313e32fee88db6807cdf2
Ubuntu <azureuser@test-lets-encrypt.rd2wr5xemyle5m2v3nbjqsn4jb.bx.internal.cloudapp.net>
2024-02-13 23:53:29 +0000

    update

--- cert-bot/start-certbot.sh
+++ cert-bot/start-certbot.sh
@@ -2,4 +2,4 @@
 
 docker build -t certbot .
 
-docker compose run --rm cert-bot-service bash -c certbot
+docker compose run --rm certbot-service bash -c "certbot certonly --webroot -w /letsencrypt -d naveenbattala.com -m nbattala@gmail.com"
